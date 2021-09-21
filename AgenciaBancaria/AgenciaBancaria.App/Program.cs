f28956802a666616cfdcf710e821d6b89524c21a
wellingtoncaires <well.caires@hotmail.com>
2021-09-20 22:14:26 -0300

    Conta Bancaria

---
+++ AgenciaBancaria/AgenciaBancaria.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua teste", "1234", "Pederneiras", "SP");
+                Cliente cliente = new Cliente("Well", "123456", "4567833", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+                Console.WriteLine("Conta criada: " + conta.Situacao + ": " +  conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                string senha = "abc123456";
+                conta.Abrir(senha);
+
+                Console.WriteLine("Conta criada: " + conta.Situacao + ": " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                conta.Sacar(10, senha);
+
+                Console.WriteLine("Saldo: " + conta.Saldo);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/ContaBancaria.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public abstract class ContaBancaria
+    {
+        public int NumeroConta { get; init; }
+        public int DigitoVerificador { get; init; }
+        public decimal Saldo { get; protected set; }
+        public DateTime? DataAbertura { get; private set; }
+        public DateTime? DataEncerramento { get; private set; }
+        public SituacaoConta Situacao { get; private set; }
+        public string Senha { get; private set; }
+        public Cliente Cliente { get; set; }
+
+        public ContaBancaria(Cliente cliente)
+        {
+            Random random = new Random();
+            NumeroConta = random.Next(50000, 100000);
+            DigitoVerificador = random.Next(0, 9);
+
+            Situacao = SituacaoConta.Criada;
+            Cliente = cliente ?? throw new Exception("Cliente deve ser informado");
+        }
+
+        public void Abrir(string senha)
+        {
+            SetaSenha(senha);
+
+            Situacao = SituacaoConta.Aberta;
+            DataAbertura = DateTime.Now;
+        }
+
+        private void SetaSenha(string senha)
+        {
+            Senha = senha.ValidaStringVazia();
+
+            if(!Regex.IsMatch(senha, @"^(?=.*?[a-z])(?=.*?[0-9]).{8,}$"))
+            {
+                throw new Exception("Senha inválida");
+            }
+
+            Senha = senha;
+        }
+
+        public virtual void Sacar(decimal valor, string senha)
+        {
+            if (Senha != senha)
+            {
+                throw new Exception("Senha inválida.");
+            }
+
+            if (Saldo < valor)
+            {
+                throw new Exception("Saldo insuficiente.");
+            }
+            Saldo -= valor;
+        }
+
+        public virtual void Depositar(decimal valor, int numeroConta, int digitoVerificador)
+        {
+            if (NumeroConta != numeroConta || DigitoVerificador != digitoVerificador)
+            {
+                throw new Exception("Conta inválida");
+            }
+
+            Console.WriteLine("Confirme os dados:");
+            Console.WriteLine($"Conta: {NumeroConta}-{DigitoVerificador}  Cliente: {Cliente.Nome}");
+            Console.Write("[S/N]: ");
+            char confirmacao = char.Parse(Console.ReadLine().Trim().ToUpper());
+
+            if (confirmacao == 'S')
+            {
+                Saldo += valor;
+            }
+            else if (confirmacao == 'N')
+            {
+                Console.WriteLine("Opreação cancelada.");
+            }
+            else
+            {
+                throw new Exception("Operação Inválida");
+            }
+        }
+    }
+
+ 
+}
